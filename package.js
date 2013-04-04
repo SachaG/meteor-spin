2a89a98cca0250f4ccfba8c9d043ab0463b9fbbd
Sacha Greif <sacha357@gmail.com>
2013-04-04 09:37:49 +0900

    config spinner in app, not in package

--- package.js
+++ package.js
@@ -3,7 +3,5 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.use('handlebars', 'client');
-
-  api.add_files(['lib/spin.js', 'lib/spin.html'], 'client');
+  api.add_files(['lib/spin.js'], 'client');
 });
