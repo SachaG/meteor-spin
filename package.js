befe762a03f07a34161d83a9363f5debbc057264
Tom Coleman <tom@thesnail.org>
2013-04-09 16:54:00 +1000

    Make sure all 3 files are included

--- package.js
+++ package.js
@@ -3,5 +3,5 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.add_files(['lib/spin.js'], 'client');
+  api.add_files(['lib/spin.js', 'lib/spinner.html', 'lib/spinner.js'], 'client');
 });
